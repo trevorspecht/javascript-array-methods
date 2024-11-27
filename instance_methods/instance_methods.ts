c837813998f6fe61f5464d7bde5b11eb24a25d87
trevorspecht <trevor.specht@gmail.com>
2024-11-27 13:34:36 -0500

    update override and add some tests

--- instance_methods/instance_methods.ts
+++ instance_methods/instance_methods.ts
@@ -26,14 +26,24 @@ export class InstanceMethods<T> extends Array<T> {
    * The concat() method of Array instances is used to merge two or more arrays.
    * This method does not change the existing arrays, but instead returns a new array.
    */
-
-  override concat(...items: unknown[]): T[] {
+  override concat(...items: any[]): any[]
+  override concat(items: (T | any[])): any[] {
     const { array } = this;
+    let merged: any[] = array;
 
     if (!items) {
-      return array;
+      return merged;
     }
 
-    return [...array, ...items];
+    if (Array.isArray(items)) {
+      merged = [...array, ...items]
+    }
+
+        // if (items[Symbol.isConcatSpreadable]) {
+    //   const toConcat = ...items;
+    //   merged = [...array, ...toConcat];
+    // }
+
+    return merged;
   }
 }
--- tests/concat.test.ts
+++ tests/concat.test.ts
@@ -1,5 +1,36 @@
+import { InstanceMethods } from "../instance_methods/instance_methods";
+
 describe('concat() method tests', () => {
-  it('merges two arrays', () => {
-    const arr = ['a', 'b', 'c', 'd'];
+  const arr = ['a', 'b', 'c', 'd'];
+  const methods = new InstanceMethods(arr);
+
+  it('does not alter original or argument arrays', () => {
+    const arr2 = [1, 2, 3];
+    const result = methods.concat(arr2);
+
+    expect(arr).toMatchObject(['a', 'b', 'c', 'd']);
+    expect(arr2).toMatchObject([1, 2, 3]);
+    expect(result).toMatchObject([...arr, ...arr2]);
+  });
+
+  it('returns original array when argument is undefined or null', () => {
+    const result = methods.concat();
+    expect(result).toBe(arr);
+  });
+
+  // it('return original array when argument is not an array', () => {
+  //   const result = methods.concat({
+  //     0: "a",
+  //     1: "b",
+  //     length: 2,
+  //   });
+  //   expect(result).toMatchObject(arr);
+  // })
+
+  it('merges two string arrays', () => {
+    const arr2 = ['x', 'y', 'z'];
+
+    const result = methods.concat(arr2);
+    expect(result).toMatchObject(['a', 'b', 'c', 'd', 'x', 'y', 'z']);
   });
 });
