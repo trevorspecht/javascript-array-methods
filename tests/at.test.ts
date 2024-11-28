396d4c0ac4a0abddbb6ebaee86b82f3de78e8b74
trevorspecht <trevor.specht@gmail.com>
2024-11-28 14:31:14 -0500

    update at tests to initialize array as InstanceMethods

--- tests/at.test.ts
+++ tests/at.test.ts
@@ -1,12 +1,12 @@
 import { InstanceMethods } from '../instance_methods/instance_methods';
 
 describe('at() tests', () => {
-  let arr = [1, 2, 3, 4, 5];
-  const methods = new InstanceMethods(arr);
+  // @ts-ignore
+  let arr: InstanceMethods<number> = [1, 2, 3, 4, 5];
 
   it('returns same as bracket notation when index >= 0', () => {
     const index = 3;
-    const result = methods.at(index);
+    const result = arr.at(index);
     const bracketResult = arr[index];
 
     expect(result).toBe(4);
@@ -15,26 +15,26 @@ describe('at() tests', () => {
 
   it('counts back from end of array if index < 0', () => {
     const index = -3;
-    const result = methods.at(index);
+    const result = arr.at(index);
 
     expect(result).toBe(3);
   });
 
   it('returns undefined if index >= array.length', () => {
     let index = 5;
-    let result = methods.at(index);
+    let result = arr.at(index);
 
     expect(result).toBeUndefined();
 
     index = 7;
-    result = methods.at(index);
+    result = arr.at(index);
 
     expect(result).toBeUndefined();
   });
 
   it('returns undefined if index < -array.length', () => {
     const index = -7;
-    const result = methods.at(index);
+    const result = arr.at(index);
 
     expect(result).toBeUndefined();
   });
